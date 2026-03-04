20aa402e739aeecd0c1aad2488aa7c13714f7a36
tristanjones-student <tristanjones@myboisestate.edu>
2026-03-03 21:19:50 -0700

    Made service for chore arrays on a schedule

--- app/backend/chore-backend/src/app.service.ts
+++ app/backend/chore-backend/src/app.service.ts
@@ -1,8 +1,62 @@
 import { Injectable } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { People } from './class/People';
+import { PeopleArray } from './class/PeopleArray';
 
 @Injectable()
 export class AppService {
-  getAllPeople() {
-    // This will return all people from current people array
+  private readonly dishChorePeople: PeopleArray;
+  private readonly dayChorePeople: PeopleArray;
+
+  constructor() {
+    this.dishChorePeople = new PeopleArray(this.buildDishPeople());
+    this.dayChorePeople = new PeopleArray(this.buildDayPeople());
+  }
+
+  private buildDishPeople(): People[] {
+    return [
+      new People('Tyson'),
+      new People('Brynlee'),
+      new People('Jerilynn/Keslee'),
+      new People('Tristan'),
+      new People('Dad'),
+      new People('Brockton/Mom'),
+    ];
+  }
+
+  private buildDayPeople(): People[] {
+    return [
+      new People('Tyson'),
+      new People('Brynlee'),
+      new People('Jerilynn/Keslee'),
+      new People('Tristan'),
+      new People('Brockton'),
+    ];
+  }
+
+  rotateDishChores(): void {
+    this.dishChorePeople.shiftPeople();
+  }
+
+  rotateDayChores(): void {
+    this.dayChorePeople.shiftPeople();
+  }
+
+  getDishChorePeople(): People[] {
+    return this.dishChorePeople.getPeopleArray();
+  }
+
+  getDayChorePeople(): People[] {
+    return this.dayChorePeople.getPeopleArray();
+  }
+
+  @Cron('0 0 * * *') // daily
+  handleDailyDishRotation(): void {
+    this.rotateDishChores();
+  }
+
+  @Cron('0 0 * * 0') // weekly
+  handleWeeklyDayRotation(): void {
+    this.rotateDayChores();
   }
 }
--- app/backend/chore-backend/src/class/PeopleArray.ts
+++ app/backend/chore-backend/src/class/PeopleArray.ts
@@ -1,4 +1,27 @@
+import { People } from './People';
+export class PeopleArray {
+  private peopleArray: People[] = [];
+  constructor(people: People[]) {
+    this.peopleArray = people;
+  }
+  
+  public addPerson(person: People): void {
+    this.peopleArray.push(person);
+  }
+  public shiftPeople(): People[] {
+    let firstElement = this.peopleArray.shift();
 
-class PeopleArray  {
-    
+    if (firstElement == undefined) {
+      console.log('array is empty');
+      return this.peopleArray;
+    }
+
+    this.peopleArray.push(firstElement);
+
+    return this.peopleArray;
+  }
+  public getPeopleArray(): People[] {
+    return this.peopleArray;
+  }
+
 }
