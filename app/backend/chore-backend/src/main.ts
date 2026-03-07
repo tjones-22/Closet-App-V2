b9154d02cc90b015e6b5128591042514a996f3b2
tristanjones-student <tristanjones@myboisestate.edu>
2026-03-06 21:17:46 -0700

    Enabled CORS for the API

--- app/backend/chore-backend/src/main.ts
+++ app/backend/chore-backend/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-require('dotenv').config({path:'src/.env'});
+require('dotenv').config({ path: 'src/.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: ['GET'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: ${process.env.PORT}`);
 }
